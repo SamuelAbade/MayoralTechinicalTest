e9d4e3d8e91f07b97a8c8e70c05d2907a2b438b8
Samuel Abade <43142125+SamuelAbade@users.noreply.github.com>
2022-01-06 14:17:34 +0100

    First Commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Techinal Test by Mayoral
+### Made with VanillaJS, CSS, HTML5 and fetched via JSON (Webscraped of mayoral.com).
+
+:ceuta_mellila:
+Español:
+
+`NOTA: Presionar 'Enter' al utilizar el searchbar, HTML5 tiene ciertas dificultades al enter el Listener onKeyPress y onKeyUp` 
+
+======
+
+:gb:
+English:
+
+`NOTE: Press 'Enter' when using the searchbar, HTML5 has some difficulties when entering the Listener onKeyPress and onKeyUp`
+
+======
+
+:brazil:
+Português:
+
+`NOTA: Pressione 'Enter' ao usar a barra de pesquisa, HTML5 tem algumas dificuldades ao entrar no Listener onKeyPress e onKeyUp`
---
+++ js/index.js
@@ -0,0 +1,78 @@
+// Make an alert
+function final(){
+    alert("Test Finalizado");
+}
+// Get all elements from data.json
+async function getJSONData() {
+    let response = await fetch('./json/data.json').catch(err => console.log(err));
+    let data = await response.json();
+    return data;
+}
+// Make searchbar works
+async function search(){
+    const search = document.getElementById('search-bar').value;
+    let dataJSON = await getJSONData();
+    // console.log(dataJSON);
+    const container = document.getElementById('product');
+    if(search == ""){
+        main();
+    }else{
+        container.innerHTML = "";
+        const filter = dataJSON.filter(data => {
+            return data.title.includes(search);
+        });
+        filter.forEach(function(product, id) {
+            const card = document.createElement('div');
+            card.classList = 'card-body';
+
+            const content = `
+                <div class="card spacing" style="width: 18rem;" id="${id}">
+                    <img class="card-img-top" src="${product.img}" alt="${product.title}">
+                    <div class="card-body">
+                        <h5 class="card-title moreTitle">${product.title}</h5>
+                        <p class="card-text" style="text-align: center;">
+                            <strike style="color: grey;">${product.oldPrice}</strike>
+                            <br/>
+                            <div class="priceTag"><b>${product.newPrice}</b></div>
+                        </p>
+                        <div class="d-flex justify-content-center">
+                            <a class="btn btn-primary btn-block" style="color: white;" onclick="final()">Añadir</a>
+                        </div>
+                    </div>
+                </div>
+            `;
+        
+    
+            container.innerHTML += content;
+        });
+    }
+}
+// Main function loaded by <body>
+async function main(){
+    let fetched = await getJSONData();
+    const container = document.getElementById('product');
+    fetched.forEach(function(product, id) {
+        const card = document.createElement('div');
+        card.classList = 'card-body';
+
+        const content = `
+            <div class="card spacing" style="width: 18rem;" id="${id}">
+                <img class="card-img-top" src="${product.img}" alt="${product.title}">
+                <div class="card-body">
+                    <h5 class="card-title moreTitle">${product.title}</h5>
+                    <p class="card-text" style="text-align: center;">
+                        <strike style="color: grey;">${product.oldPrice}</strike>
+                        <br/>
+                        <div class="priceTag"><b>${product.newPrice}</b></div>
+                    </p>
+                    <div class="d-flex justify-content-center">
+                        <a class="btn btn-primary btn-block" style="color: white;" onclick="final()">Añadir</a>
+                    </div>
+                </div>
+            </div>
+        `;
+        
+    
+        container.innerHTML += content;
+    });
+}
